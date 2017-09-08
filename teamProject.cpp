a32a29908228ecd546310e08bee7ef933fd7ffe4
ypd00085 <ypd00085@naver.com>
2017-09-08 15:19:42 +0900

    add teamProject.cpp

---
+++ teamProject.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main(){
+	
+	string name1 = "20122969 이동환";
+	string name2 = "";
+	string name3 = "";
+	string name4 = "";
+	string name5 = "";
+	
+	cout<<"저희 6조의 구성원은 " << name1 <<", "<< name2 << ", "<< name3 << ", "<< name4 <<  ", " << name5 <<" 이렇게 총 5명 입니다";
+	
+	
+}
