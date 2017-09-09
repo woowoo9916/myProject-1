237a6d126e2d6acb7238663253b767c9a08f890a
jjaewon93 <sosa306@naver.com>
2017-09-09 18:27:28 +0900

    Update teamProject.cpp

--- teamProject.cpp
+++ teamProject.cpp
@@ -8,7 +8,7 @@ int main(){
 	string name1 = "20122969 이동환";
 	string name2 = "";
 	string name3 = "";
-	string name4 = "";
+	string name4 = "20123025 정재원";
 	string name5 = "20123028 허재준";
 	
 	cout<<"저희 6조의 구성원은 " << name1 <<", "<< name2 << ", "<< name3 << ", "<< name4 <<  ", " << name5 <<" 이렇게 총 5명 입니다";
